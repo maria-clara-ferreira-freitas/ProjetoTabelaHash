5b9ddafc82d8305e45ae4ba7d4a07eceb4abe659
maria.clara.ferreira.freitas <maria.clara.ferreira.freitas@ccc.ufcg.edu.br>
2025-03-12 19:47:51 -0300

    interface e hash multiplication

---
+++ src/main/java/TabelaHashMultiplication.java
@@ -0,0 +1,48 @@
+public class TabelaHashMultiplication implements TabelaHashProjeto{
+
+    private Integer[] tabela;
+    private int tamanho;
+    private int colisoes;
+    private static final Integer DELETED = Integer.MIN_VALUE;
+    private double fatorDeCarga;
+
+    public static final int CAPACIDADE_DEFAULT = 1500;
+
+    public static final double FATOR_DE_CARGA_DEFAULT = 0.85;
+
+    @Override
+    public int hash(Integer chave) {
+        return chave % this.tabela.length;
+    }
+
+    private int funcaoHashMultiplicacao(Integer chave){
+        double a = 0.6180339887; // utilizando a constante sugerida por Knuth
+        double hash = ((chave * a) % 1) * this.tabela.length;
+        return (int)hash;
+    }
+
+    @Override
+    public void add(Integer chave) {
+
+    }
+
+    @Override
+    public Integer remove(Integer chave) {
+        return 0;
+    }
+
+    @Override
+    public void resize() {
+
+    }
+
+    @Override
+    public boolean contains(Integer chave) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return this.tamanho;
+    }
+}
---
+++ src/main/java/TabelaHashProjeto.java
@@ -0,0 +1,13 @@
+public interface TabelaHashProjeto {
+    public int hash(Integer chave);
+
+    public void add(Integer chave);
+
+    public Integer remove(Integer chave);
+
+    void resize();
+
+    public boolean contains(Integer chave);
+
+    public int size();
+}
