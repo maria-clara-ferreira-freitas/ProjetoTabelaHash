c089141535e84e97872f7802ef44b2d2d5fb99ca
maria.clara.ferreira.freitas <maria.clara.ferreira.freitas@ccc.ufcg.edu.br>
2025-03-13 18:37:30 -0300

    ajuste main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,9 @@
-import java.util.ArrayList;
 import java.io.*;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         TabelaHashMultiplication hashTable = new TabelaHashMultiplication(3000, 0.85);
-        String arquivo = "data/random_numbers3.txt"; // ou o caminho completo, como "/caminho/do/arquivo/random_numbers.txt"
+        String arquivo = "data/random_numbers3.txt";
 
         try {
             BufferedReader reader = new BufferedReader(new FileReader(arquivo));
@@ -14,26 +12,20 @@ public class Main {
             while ((line = reader.readLine()) != null) {
                 String[] tokens = line.split(" ");
 
-                // Medindo tempo de execução
                 long start = System.nanoTime();
 
-                // Loop para adicionar os números na tabela de hash
                 for (String token : tokens) {
                     try {
-                        int chave = Integer.parseInt(token); // Converte o token para número inteiro
-                        hashTable.add(chave); // Adiciona à tabela de hash
+                        int chave = Integer.parseInt(token);
+                        hashTable.add(chave);
                     } catch (NumberFormatException e) {
-                        // Ignora caso algum token não seja um número válido
                         System.out.println("Token inválido: " + token);
                     }
                 }
 
             }
 
-            // Exibe o número total de colisões após o processamento do arquivo
             System.out.println("Número total de colisões: " + hashTable.getColisoes());
-
-            // Fecha o BufferedReader após ler o arquivo
             reader.close();
 
         } catch (IOException ioe) {
